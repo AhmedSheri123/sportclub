87d97d018b74fd628ab3486947d7b8f1082e2b87
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2024-08-11 00:52:48 +0300

    add_CoachAppointments_model

--- club_dashboard/urls.py
+++ club_dashboard/urls.py
@@ -4,12 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
 
-    path('addClub', views.addClub, name="addClub"),
-    path('editClub/<int:id>', views.editClub, name="editClub"),
-    path('viewClub', views.viewClub, name="viewClub"),
-
-
-    path('addDirector', views.addDirector, name="addDirector"),
-    path('editDirector/<int:id>', views.editDirector, name="editDirector"),
-    path('viewDirector', views.viewDirector, name="viewDirector"),
 ]
