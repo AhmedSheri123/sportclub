5e2a3911523ef092173922aa29d59616b963a8df
Adam Stitou <adamkoora27@gmail.com>
2024-08-20 15:07:01 +0100

    student UI and products base UI

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='studentIndex')
+    path('', views.index, name='studentIndex'),
+    path('viewProducts', views.viewProducts, name='viewProducts'),
+    path('viewProducts/<int:id>', views.viewSpecific, name='viewSpecific')
 ]
--- students/views.py
+++ students/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'templates/student/index.html')
+    return render(request, 'student/index.html')
+
+def viewProducts(request): 
+    return render(request, 'student/products/viewProducts.html')
+
+def viewSpecific(request): 
+    return render(request, 'student/products/viewSpecific.html')
